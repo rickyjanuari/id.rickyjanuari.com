2173b413cc6b1b8cc98862702a275ccf17ec66c5
Ricky Januari <5415908+rickyjanuari@users.noreply.github.com>
2024-11-28 21:38:17 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown';
+
 
 const SITE_URL = "https://id.rickyjanuari.com";
 
@@ -10,7 +10,6 @@ export default defineConfig({
   base: '/', // Ensures assets use root path
   integrations: [
     sitemap(),
-    partytown(),
     tailwind({
       config: { applyBaseStyles: false },
     }),
