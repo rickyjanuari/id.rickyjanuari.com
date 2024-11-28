d87d6ce79e4054b087edd3d8913e1648bdf93e50
Ricky Januari <5415908+rickyjanuari@users.noreply.github.com>
2024-11-28 21:33:11 +0700

    add google anality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 const SITE_URL = "https://id.rickyjanuari.com";
 
@@ -9,6 +10,7 @@ export default defineConfig({
   base: '/', // Ensures assets use root path
   integrations: [
     sitemap(),
+    partytown(),
     tailwind({
       config: { applyBaseStyles: false },
     }),
